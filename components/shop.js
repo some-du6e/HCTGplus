1434d4f9f6a243109b1ebe57c1bbb9a68a93785e
some-du6e <103129948+some-du6e@users.noreply.github.com>
2026-04-10 15:09:18 -0600

    imma do smth different next commit trust, this has a lot of flaws and shi

--- components/shop.js
+++ components/shop.js
@@ -22,12 +22,54 @@ function betterShop() {
 
 
     // shop ui now...
+
+    // get shopcontainer
     let shopcontainer = null
     let possibleshopcontainers = document.getElementsByClassName("grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3") 
     // i think this is bs cuz i think its never gonna change
     for (let posibility of possibleshopcontainers ) {
-        
+        if (posibility.children[0].className === "flex h-full flex-col") {
+            shopcontainer = posibility
+            break
+        }
     }
+    if (!shopcontainer) {
+        console.error("HCTG: could not find shop container! ID: 1s9f8g")
+        return
+    }
+
+
+    // keep first item as template, remove the rest
+    while (shopcontainer.children.length > 1) {
+        shopcontainer.removeChild(shopcontainer.lastChild)
+    }
+
+    const itemTemplate = shopcontainer.children[0]
+    itemTemplate.hidden = true
+    function renderitems() {
+        let items = window.HCTG.datapage.props.items
+        for (let item of items) { 
+            let chud = itemTemplate.cloneNode(true)
+
+            // featured?
+            if (!item.featured) {
+                chud.children[0].children[0].hidden = true
+            }
+
+            // image
+            let imgthing = chud.children[1].children[0]
+            imgthing.src = item.image
+
+            // title
+            let titlething = chud.children[1].children[1].children[0]
+            titlething.innerText = item.name
+
+            chud.hidden = false
+            shopcontainer.appendChild(chud)
+        }
+    }
+    renderitems()
+
 }
 
 window.addEventListener('pageChange', function() {
